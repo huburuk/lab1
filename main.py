869cd2f239250808fe04895695ba76d1cb91e666
huburuk <79721194+huburuk@users.noreply.github.com>
2021-02-27 01:30:17 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+
+def main():
+    logging.info("hello world")
+
+
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
