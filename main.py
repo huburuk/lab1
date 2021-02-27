64c2269b20ba9d659b748b7f5ffc4c17e0ab6299
huburuk <huburuk@mail.ru>
2021-02-27 11:26:41 +0300

    init comiting

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ logging.basicConfig(level=logging.INFO)
 
 def main():
     logging.info("hello world")
+    def sum(term1, term2):
+        return term1 + term2
+    logging.info(sum(3, 4))
 
 
 if __name__ == '__main__':
