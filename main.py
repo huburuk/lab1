520c9a208217fa88b055da0b5ebd21dc4d5f95b4
huburuk <79721194+huburuk@users.noreply.github.com>
2021-03-13 08:39:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
+#!/usr/bin/env python3
 import logging
+import random
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    logging.info("введите число a:")
-    a = input()
-    logging.info("введите число b:")
-    b = input()
-    if a > b:
-        logging.info("вы тупой")
-        return 0
-    logging.info("загадайте рандомное число от a до b:")
-    number1 = input()
-    if number1 > b or number1 < a:
-        logging.info("введите другое число:")
-        number1 = input()
-        if number1 > b or number1 < a:
-            logging.info("вы тупой")
-            return 0
+    logging.info("загадано рандомное число от 0 до 100:")
+    number1 = random.randint(0, 100)
     for i in range(3):
         logging.info("введите ваше число:")
-        number2 = input()
+        number2 = int(input())
         if (number1 > number2) or (number1 < number2):
             logging.info("вы не угадали")
             if i == 2:
