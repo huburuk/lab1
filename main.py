4c02305437c974550121a562d9377b9fc436e5d9
huburuk <huburuk@mail.ru>
2021-03-27 10:58:45 +0200

    New version of game

--- main.py
+++ main.py
@@ -6,25 +6,31 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    logging.info("загадано рандомное число от 0 до 100:")
+    logging.info("загадано рандомное число от 0 до 100")
     number1 = random.randint(0, 100)
-    for i in range(3):
-        logging.info("введите ваше число:")
-        number2 = int(input())
-        if (number1 > number2) or (number1 < number2):
+    for counter in range(3):
+        while True:
+            try:
+                number2 = int(input("введите ваше число:"))
+                break
+            except ValueError:
+                logging.info("Ошибка!")   
+        if number1 > number2 or number1 < number2:
             logging.info("вы не угадали")
-            if i == 2:
+            if counter == 2:
                 logging.info("Попытки кончились(")
-                return 0
+                break
             if number2 < number1:
                 logging.info("ваше число меньше нужного, повторите попытку...")
             elif number2 > number1:
                 logging.info("ваше число больше нужного, повторите попытку...")
         else:
             logging.info("Вы угадали))")
-            return 0
+            break
 
 
 if __name__ == '__main__':
-    main()
-
+    try:
+        main()
+    except KeyboardInterrupt:
+        logging.error("Ошибка!")
